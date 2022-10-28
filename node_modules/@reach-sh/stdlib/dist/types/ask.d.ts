ed600d8df08f8e839a0399990ea9df908c941ee6
xiaupeng <86650877+xiaupeng@users.noreply.github.com>
2022-10-28 15:20:04 +0800

    Revert "v1.1"
    This reverts commit 7aa1c085b361f9ca05052f5319eb0cbbb8976dbb.

--- node_modules/@reach-sh/stdlib/dist/types/ask.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const ask: <T>(question: string, validator?: ((s: string) => T) | undefined) => Promise<T>;
-export declare const done: () => void;
-export declare const yesno: (answer: string) => boolean;
-//# sourceMappingURL=ask.d.ts.map
--- node_modules/@reach-sh/stdlib/dist/types/runner.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function run(file: string, ...args: Array<string>): Promise<void>;
-export declare function main(): void;
-//# sourceMappingURL=runner.d.ts.map
